eae0ed95df0adc6db63907c225c18231d574b5c7
j c <thldesignz2@gmail.com>
2021-06-29 21:13:06 -0400

    added hotjar

--- pages/index.js
+++ pages/index.js
@@ -54,6 +54,17 @@ const HomePage =() => {
                       }();
                       drift.SNIPPET_VERSION = '0.3.1';
                       drift.load('6wsbrn3ipcm4');
+
+
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2479348,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
                                     })
 
 
