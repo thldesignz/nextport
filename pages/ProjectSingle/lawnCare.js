f1630b198dee81c84548b2345752564bfdcc1f96
bruce.slaughter <bruce.slaughter@smail.rasmussen.edu>
2022-04-20 12:26:21 -0400

    updated  link

--- pages/ProjectSingle/lawnCare.js
+++ pages/ProjectSingle/lawnCare.js
@@ -29,7 +29,7 @@ const ProjectSingle = ( props ) => {
                                 <div className="co-l-lg-4 col-md-4 col-sm-6 col-12">
                                     <div className="wpo-project-details-text">
                                         <span>Project URL</span>
-                                        <h2><a target="_blank" href="https://lawn-care.herokuapp.com/">Click Here</a></h2>
+                                        <h2><a target="_blank" href="https://new-lawncare.herokuapp.com/">Click Here</a></h2>
                                     </div>
                                 </div>
                             </div>
